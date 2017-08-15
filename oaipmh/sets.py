c5ceb5e2517d5e804b12d7ff7213463a8763de86
Gustavo Fonseca <gustavo@gfonseca.net>
2017-08-15 18:05:39 -0300

    Especificação básica para a implementação dos sets

---
+++ oaipmh/sets.py
@@ -0,0 +1,16 @@
+"""
+Deve ser possível:
+  * Estruturar os conjuntos de forma hierárquica;
+  * Acrescentar novos conjuntos sem que haja refatoração; Design para extensão;
+  * Representar conjuntos dinâmicos (aqueles que podem ser acrescentados
+    durante a execução do programa como resultado da inclusão de novos registros.
+    São conjuntos que agrupam registros pelo valor de determinado atributo.
+    e.g.: ISSN, tipo de documento, ano de publicação etc);
+  * Representar conjuntos estáticos (definidos no momento da compilação pelo
+    programador);
+  * Listar os diversos conjuntos disponibilizados pelo repositório;
+  * Sinalizar nos registros os conjuntos os quais estão contidos;
+  * Consultar os registros contidos em um conjunto;
+  
+"""
+
